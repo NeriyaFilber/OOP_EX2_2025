59c68e17e5bb8340997b28182c0918a451c197db
NeriyaFilber <160753463+NeriyaFilber@users.noreply.github.com>
2024-12-05 00:08:08 +0200

    create session factory

---
+++ gym/management/Sessions/SessionFactory.java
@@ -0,0 +1,19 @@
+package gym.management.Sessions;
+
+import gym.management.Instructor;
+
+public class SessionFactory {
+    public static Session createSession(SessionType sessionType, String s, ForumType forumType, Instructor instructor){
+        switch (sessionType){
+            case Ninja:
+                return new Ninja(s,forumType,instructor);
+            case Pilates:
+                return new Pilates(s,forumType,instructor);
+            case ThaiBoxing:
+                return new ThaiBoxing(s,forumType,instructor);
+            case MachinePilates:
+                return new MachinePilates(s,forumType,instructor);
+        }
+        return null;
+    }
+}
