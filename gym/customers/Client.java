340f0f89f71a5c4139b0a492bf2ed0e3edd51381
NeriyaFilber <160753463+NeriyaFilber@users.noreply.github.com>
2024-12-05 00:06:59 +0200

    add constructor and get notifications

--- gym/customers/Client.java
+++ gym/customers/Client.java
@@ -3,10 +3,17 @@ package gym.customers;
 import gym.management.Gender;
 import gym.management.Person;
 
+import java.util.ArrayList;
+
 public class Client extends Person {
+    private ArrayList<String> _notifications = new ArrayList<>();
 
     public Client(String name, int balance, Gender gender, String dateOfBirth) {
         super(name, balance, gender, dateOfBirth);
+    }
+
+    public Client(Person p){
+        super(p.getName(),p.getBalance(),p.getGender(), p.getDateOfBirth());
     }
 
     public String getName() {
@@ -14,6 +21,16 @@ public class Client extends Person {
     }
 
     public String getNotifications() {
-        return "";
+        if (_notifications.isEmpty()){
+            return "[]";
+        }
+        StringBuilder ans = new StringBuilder("[");
+        for (String not: _notifications) {
+            ans.append(not);
+            ans.append(",");
+        }
+        ans.deleteCharAt(ans.length()-1); //remove the last char ","
+        ans.append("]");
+        return ans.toString();
     }
 }
