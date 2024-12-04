805ec522f3442520fb2e5c3fbe79c13856b0e97b
shiraDuek <shira.duek123@gmail.com>
2024-12-04 18:34:48 +0200

    ש
    Co-Authored-By: NeriyaFilber <160753463+neriyafilber@users.noreply.github.com>

--- gym/management/Gym.java
+++ gym/management/Gym.java
@@ -1,5 +1,10 @@
 package gym.management;
 
+import gym.Exception.InstructorNotQualifiedException;
+import gym.management.Sessions.ForumType;
+import gym.management.Sessions.Session;
+import gym.management.Sessions.SessionType;
+
 public class Gym {
     private static Gym gym = new Gym();
     private static Secretary secretary;
